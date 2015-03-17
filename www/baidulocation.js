cd1307acedea7a1c254567097284f4d7b59f7e8c
林军 <lj@strongsoft.net>
2015-03-17 09:47:23 +0800

    new file

---
+++ www/baidulocation.js
@@ -0,0 +1,10 @@
+var exec = require('cordova/exec');
+module.exports = {
+   startLocation: function(success, error, options) {
+       options = options || {};
+       var coorType = options.CoorType ? options.CoorType : 'bd09ll';
+       var isNeedAddress = options.IsNeedAddress ? options.IsNeedAddress : false;
+       var args = [coorType, isNeedAddress];
+       exec(success, error, "BaiduLocation4Cordova", "startLocation", args);
+   }
+};
