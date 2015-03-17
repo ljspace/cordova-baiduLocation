bb52ed2b63ee454465a4f4906ef9308bbe98a04e
林军 <lj@strongsoft.net>
2015-03-17 10:12:18 +0800

    调整方法

--- www/baidulocation.js
+++ www/baidulocation.js
@@ -1,10 +1,30 @@
-var exec = require('cordova/exec');
-module.exports = {
-   startLocation: function(success, error, options) {
-       options = options || {};
-       var coorType = options.CoorType ? options.CoorType : 'bd09ll';
-       var isNeedAddress = options.IsNeedAddress ? options.IsNeedAddress : false;
-       var args = [coorType, isNeedAddress];
-       exec(success, error, "BaiduLocation", "startLocation", args);
-   }
-};
+window.locationService = {
+	execute: function(action, successCallback, errorCallback) {
+		cordova.exec(    
+			function(pos) {
+				var errcode = pos.code;
+				if (errcode == 61 || errcode == 65 || errcode == 161) {
+					successCallback(pos);
+				} else {
+					if (typeof(errorCallback) != "undefined") {
+						if (errcode >= 162) {
+							errcode = 162;
+						}
+						errorCallback(pos)
+					};
+				}
+			}, 
+			function(err){},
+			"BaiduLocation",
+			action,
+			[]
+		)
+	},
+	getCurrentPosition: function(successCallback, errorCallback) {
+		this.execute("getCurrentPosition", successCallback, errorCallback);
+	},
+	stop: function(action, successCallback, errorCallback) {
+		this.execute("stop", successCallback, errorCallback);
+	}
+}
+module.exports = locationService;
